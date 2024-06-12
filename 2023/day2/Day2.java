5c79ba0c65911cefd000d7f0ec5251fd4bc0b2ed
Coleman Wimpy <coleman.wimpy@protonmail.com>
2024-06-11 22:14:09 -0400

    Day2 part 2

--- 2023/day2/Day2.java
+++ 2023/day2/Day2.java
@@ -16,25 +16,36 @@ public class Day2 {
 
       List<String> gameList = new ArrayList<String>();
       int part1Result = 0;
+      int part2Result = 0;
 
       while (scanner.hasNext()) {
         gameList.add(scanner.nextLine().split(":")[1]);
       }
 
       for (int i = 0; i < gameList.size(); i++) {
-        if (isGameValid(gameBag, gameList.get(i)))
+        Map.Entry<Boolean, Integer> resultPair = isGameValid(gameBag, gameList.get(i));
+        if (resultPair.getKey())
           part1Result += i + 1;
+
+        part2Result += resultPair.getValue();
       }
 
       System.out.println("Part 1: " + part1Result);
+      System.out.println("Part 2: " + part2Result);
       scanner.close();
     } catch (Exception e) {
       System.out.println(e);
     }
   }
 
-  static Boolean isGameValid(HashMap<String, Integer> gameBag, String gameData) {
+  static Map.Entry<Boolean, Integer> isGameValid(HashMap<String, Integer> gameBag, String gameData) {
     String[] draws = gameData.replaceAll(";", ",").split(",");
+    HashMap<String, Integer> minBagContents = new HashMap<>();
+    minBagContents.put("red", 0);
+    minBagContents.put("blue", 0);
+    minBagContents.put("green", 0);
+    int gamePower = 1;
+    boolean gamePossible = true;
 
     for (String it : draws) {
       try {
@@ -42,13 +53,20 @@ public class Day2 {
         int count = Integer.parseInt(draw[0]);
         String color = draw[1];
         if (count > gameBag.get(color))
-          return false;
+          gamePossible = false;
+
+        if (minBagContents.get(color) < count)
+          minBagContents.replace(color, count);
       } catch (Exception e) {
         System.out.println(it);
         System.out.println(e);
       }
     }
 
-    return true;
+    for (int number : minBagContents.values()) {
+      gamePower *= number;
+    }
+
+    return new AbstractMap.SimpleEntry<Boolean, Integer>(gamePossible, gamePower);
   }
 }
