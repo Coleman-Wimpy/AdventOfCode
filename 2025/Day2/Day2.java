e1d4c82bb1896acce7725c89f9bf12c9a9b3205c
Coleman-Wimpy <coleman.wimpy@protonmail.com>
2025-12-05 11:54:16 -0500

    Adding 2025 Day 2 Part 2

--- 2025/Day2/Day2.java
+++ 2025/Day2/Day2.java
@@ -2,11 +2,13 @@ package Day2;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day2 {
 
     static Long invalidIdTotalValue = 0L;
+    static Long invalidIdTotalValueV2 = 0L;
 
     public static void main(String[] args) {
         try {
@@ -17,10 +19,15 @@ public class Day2 {
 
             for (String idRange : idStrings) {
                 checkForInvalidIds(idRange);
+                checkForInvalidIdsV2(idRange);
             }
 
             System.out.println(
                 "Invalid ID Total Value: " + invalidIdTotalValue
+            );
+
+            System.out.println(
+                "Invalid ID Total Value V2: " + invalidIdTotalValueV2
             );
 
             fileReader.close();
@@ -44,5 +51,52 @@ public class Day2 {
 
             if (left.equals(right)) invalidIdTotalValue += i;
         }
+    }
+
+    private static void checkForInvalidIdsV2(String idRange) {
+        String[] idRanges = idRange.split("-");
+        Long start = Long.parseLong(idRanges[0]);
+        Long end = Long.parseLong(idRanges[1]);
+
+        for (Long i = start; i <= end; i++) {
+            String id = i.toString();
+            boolean isInvalidId = checkArrayHasAllDuplicates(id);
+            if (isInvalidId) invalidIdTotalValueV2 += i;
+        }
+    }
+
+    private static boolean checkArrayHasAllDuplicates(String id) {
+        // System.out.println("Checking ID: " + id);
+        for (int j = 1; j < id.length() + 1; j++) {
+            boolean isInvalidId = true;
+            String digit = "";
+            ArrayList<String> digits = new ArrayList<>();
+
+            for (char c : id.toCharArray()) {
+                digit += c;
+                if (digit.length() == j) {
+                    digits.add(digit);
+                    digit = "";
+                }
+            }
+
+            if (!digit.isEmpty()) digits.add(digit);
+
+            if (digits.size() < 2) return false;
+
+            String firstDigit = digits.get(0);
+            //System.out.println("Digit Array: " + digits);
+            for (String currentDigit : digits) {
+                if (!firstDigit.equals(currentDigit)) {
+                    isInvalidId = false;
+                    break;
+                }
+            }
+            if (isInvalidId) {
+                System.out.println("ID is invalid: " + id);
+                return true;
+            }
+        }
+        return false;
     }
 }
